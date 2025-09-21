b6a56066b73c9f44454a28fad781511a37f351d2
Esat Gokcen <esatgokcen173@gmail.com>
2025-09-21 15:35:23 +0300

    added new tmdb file

---
+++ server/src/services/tmdb.ts
@@ -0,0 +1,53 @@
+import { tmdb } from "../lib/http";
+import { memoize } from "../lib/cache";
+
+
+export type TmdbConfiguration = {
+images: {
+base_url: string;
+secure_base_url: string;
+backdrop_sizes: string[];
+logo_sizes: string[];
+poster_sizes: string[];
+profile_sizes: string[];
+still_sizes: string[];
+};
+};
+
+
+export async function getConfiguration(): Promise<TmdbConfiguration> {
+return memoize("tmdb:configuration", async () => {
+const { data } = await tmdb.get<TmdbConfiguration>("/configuration");
+return data;
+}, 24 * 60 * 60 * 1000);
+}
+
+
+export async function searchMovies(query: string, region?: string) {
+const { data } = await tmdb.get("/search/movie", {
+params: {
+query,
+include_adult: false,
+region,
+},
+});
+return data;
+}
+
+
+export async function getMovieDetails(id: string | number, _region?: string) {
+// region can affect release dates; not strictly needed for details here
+const { data } = await tmdb.get(`/movie/${id}`, {
+params: {
+append_to_response: "credits,images",
+include_image_language: "en,null",
+},
+});
+return data;
+}
+
+
+export async function getWatchProviders(id: string | number) {
+const { data } = await tmdb.get(`/movie/${id}/watch/providers`);
+return data;
+}
