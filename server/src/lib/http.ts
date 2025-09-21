1bc62c11b851c4cbaefa953743537cadab5ba52e
Esat Gokcen <esatgokcen173@gmail.com>
2025-09-21 15:34:31 +0300

    added middleware http files

---
+++ server/src/lib/http.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { env } from "../config/env";
+
+
+export const tmdb = axios.create({
+baseURL: "https://api.themoviedb.org/3",
+params: { api_key: env.TMDB_API_KEY },
+timeout: 15_000,
+});
