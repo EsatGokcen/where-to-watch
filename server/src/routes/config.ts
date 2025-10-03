9d1d8bf753c1802340f919c93fe5c3e16f48b326
Esat Gokcen <esatgokcen173@gmail.com>
2025-10-03 17:31:20 +0300

    added new function to config file with a manual allow list

--- server/src/routes/config.ts
+++ server/src/routes/config.ts
@@ -1,16 +1,37 @@
 import { Router } from "express";
-import { getConfiguration } from "../services/tmdb";
+import { getConfiguration, getWatchProviderRegions } from "../services/tmdb";
 
 const r = Router();
+const ALLOW = new Set([
+  "US",
+  "GB",
+  "TR",
+  "CA",
+  "AU",
+  "DE",
+  "FR",
+  "ES",
+  "IT",
+  "IN",
+  "BR",
+  "JP",
+]);
 
 r.get("/", async (_req, res, next) => {
   try {
     const cfg = await getConfiguration();
+    const regions = await getWatchProviderRegions();
+
+    const supported = regions?.results
+      ?.map((x: any) => x.iso_3166_1)
+      ?.filter((code: string) => ALLOW.has(code)) // or remove this line to allow ALL
+      ?.sort() ?? ["US", "GB", "TR"];
+
     res.json({
       images: cfg.images,
       attribution:
         "This product uses the TMDB API but is not endorsed or certified by TMDB. Streaming availability data courtesy of JustWatch.",
-      supportedRegions: ["US", "GB", "TR"],
+      supportedRegions: supported,
     });
   } catch (err) {
     next(err);
